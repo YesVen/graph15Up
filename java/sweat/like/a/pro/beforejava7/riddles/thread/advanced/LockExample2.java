5ea3d4c980b36ebd8cb2e6f64f886634d82d7669
Dnyanesh Barkade <dbe1@3ds.com>
2017-06-17 14:44:52 +0530

    Code

---
+++ java/sweat/like/a/pro/beforejava7/riddles/thread/advanced/LockExample2.java
@@ -0,0 +1,40 @@
+
+package sweat.like.a.pro.beforejava7.riddles.thread.advanced;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockExample2 {
+	public static void main(String[] args) {
+		
+		final Lock lock = new ReentrantLock();
+		
+		Thread t1 = new Thread(()-> {
+				run(lock);
+		}, "t1");
+		
+		Thread t2 = new Thread(()-> {
+			run(lock);
+		}, "t2");
+		
+		t1.start();
+		t2.start();
+	}
+
+	private static void run(Lock lock) {
+		do{
+			if(lock.tryLock())
+			{
+				System.out.println(Thread.currentThread().getName() + " performing safe operation");
+				lock.unlock();
+				break;
+			}
+			else
+			{
+				System.out.println(Thread.currentThread().getName() + " performing alternative operation. Will try for getting lock too.");
+			}
+		}while(true);
+	}
+	
+	
+}
