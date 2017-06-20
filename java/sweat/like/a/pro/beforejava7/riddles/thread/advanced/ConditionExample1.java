f202b826d11c927ceb0065565afa6be51e472de5
dnyaneshlb <dbe1@3ds.com>
2017-06-20 15:50:18 +0530

    Code
    Code

---
+++ java/sweat/like/a/pro/beforejava7/riddles/thread/advanced/ConditionExample1.java
@@ -0,0 +1,18 @@
+package sweat.like.a.pro.beforejava7.riddles.thread.advanced;
+/**
+ * 	This class is wrapper for ootb wait/notify framework
+ *  
+ *	It provides rich methods for checking conditions while thread execution
+ */
+public class ConditionExample1 {
+	public static void main(String[] args) {
+		
+		boolean isT1Died = false;
+		
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i < 5; i++) {
+				
+			}
+		},"t1");
+	}
+}
