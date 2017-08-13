0ac7202cfdaa29d8b1f8e09125c1a5830fb33646
Dnyanesh Barkade <dbe1@3ds.com>
2017-08-13 23:43:50 +0530

    Code
    Code

--- Spring/mvc/src/main/java/sweat/like/a/pro/controller/RawMaterialController.java
+++ Spring/mvc/src/main/java/sweat/like/a/pro/controller/RawMaterialController.java
@@ -1,10 +1,7 @@
 package sweat.like.a.pro.controller;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
-=======
+
 import java.io.IOException;
->>>>>>> origin/master
 import java.util.List;
 import java.util.Map;
 
@@ -14,14 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import sweat.like.a.pro.customdataeditor.CustomNameEditor;
--- Spring/mvc/src/main/java/sweat/like/a/pro/controller/SimpleRestController.java
+++ Spring/mvc/src/main/java/sweat/like/a/pro/controller/SimpleRestController.java
@@ -3,10 +3,15 @@ package sweat.like.a.pro.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import sweat.like.a.pro.model.RawMaterial;
 
@@ -21,11 +26,14 @@ public class SimpleRestController
 {
 	  
 	/*
-	 * In order to use @reponseBody annotation we need jackson jars (for data conversion)
+	 * This method supports both xml and json formats as we have not mentioned @produces annotation here
+	 * If we want to support only one format then use @produces annotation
 	 * 
-	 *  This way we can skip the redundant MOdelAndView Object in return type
+	 * 
+	 * In order to use @reponseBody annotation we need jackson jars (for data conversion)
+	 * This way we can skip the redundant MOdelAndView Object in return type
 	 */
-	@RequestMapping(value="/rawmaterials/{rmName}")
+	@RequestMapping(value="/rawmaterials/{rmName}"/*, produces=MediaType.APPLICATION_XML_VALUE*/)
 	public List<RawMaterial> getRawMaterials(@PathVariable("rmName") String pathItem)
 	{
 		System.out.println("Referenced URI paramter is " + pathItem);
@@ -37,4 +45,33 @@ public class SimpleRestController
 		list.add(rawMaterial);
 		return list;
 	}
+	
+	
+	/**
+	 * Run this service using postman  : http://localhost:8080/SpringMVC/rawmaterials/fabrics
+	 * Set Content type to application/json
+	 * add body as 
+	 * 			{
+					"raw_material_name" : "BF-01",
+					"vendor" : "Sai Fabrication"
+				}
+	 */
+	@RequestMapping(value="/rawmaterials/fabrics", method={RequestMethod.POST} )
+	public ResponseEntity<Boolean> getFabrics(@RequestBody RawMaterial rawMaterial)
+	{
+		System.out.println(rawMaterial.getRawMaterialName());
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		//use HATEOAS
+		String location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/"+rawMaterial.getRawMaterialName()).toUriString();
+		
+		headers.add("Location", location);
+		headers.add("PoweredBy", "HackerEverywhere");
+		
+		return new ResponseEntity<Boolean>(headers,HttpStatus.CREATED);
+	}
+	
+	
+	
 }
