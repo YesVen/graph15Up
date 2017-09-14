008320b8641ee500fc82f8646e714071923c2b5a
Dnyanesh Barkade <dbe1@3ds.com>
2017-09-14 09:25:24 +0530

    code
    code

--- java/sweat/like/a/pro/beforejava7/riddles/collection/EnumerationExample.java
+++ java/sweat/like/a/pro/beforejava7/riddles/collection/EnumerationExample.java
@@ -3,7 +3,23 @@ package sweat.like.a.pro.beforejava7.riddles.collection;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Vector;
-
+/**
+ * Enumeration is a legacy interface used to traverse only the legacy classes like Vector, HashTable and Stack
+ * 
+ * Iterator is a fail-fast in nature. i.e it throws ConcurrentModificationException if a collection is modified while iterating other than it’s own remove() method. Where as Enumeration is fail-safe in nature. It doesn’t throw any exceptions if a collection is modified while iterating. 
+ * 
+ * As Iterator is fail-fast in nature and doesn’t allow modification of a collection by other threads while iterating, it is considered as safe and secure than Enumeration.
+ * 
+ * Enumeration is 2x faster. 
+ * 
+ * 
+ * itr jdk 1.2
+ * enumeartiion jdk 1.0
+ * 
+ * 
+ * @author Nightfury
+ *
+ */
 public class EnumerationExample {
 	public static void main(String[] args) {
 		Vector<Integer> numbers = new Vector<>();
