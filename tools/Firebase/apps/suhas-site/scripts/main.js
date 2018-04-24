3edc32dc4af77dddc3fbd51bbd26f961611a67e6
Barkade (EXT) <yr9kvt@splm.siemens.com>
2018-04-24 11:12:27 +0530

    Code
    Code

---
+++ tools/Firebase/apps/suhas-site/scripts/main.js
@@ -0,0 +1,15 @@
+(function(){
+    
+    $("#scrollDownButton").on('click', function(e){
+       if(this.hash !== ""){
+           e.preventDefault();
+           var hash = this.hash;
+           
+           $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+       } 
+    });
+})();
